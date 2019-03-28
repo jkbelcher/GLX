d53329c1ad147283df3143b505b780f1de166a91
Mark Slee <mcslee@gmail.com>
2019-03-27 17:42:23 -0700

    Add selection focus color

--- src/main/java/heronarts/glx/ui/UITheme.java
+++ src/main/java/heronarts/glx/ui/UITheme.java
@@ -50,6 +50,7 @@ public class UITheme {
   private int recordingColor = 0xffa00044;
   private int cursorColor = 0xff555555;
   private int selectionColor = 0xff333333;
+  private int focusSelectionColor = 0xff393939;
   private int errorColor = 0xffff0000;
 
   private int darkBackgroundColor = 0xff191919;
@@ -526,6 +527,15 @@ public class UITheme {
   public UITheme setSelectionColor(int color) {
     this.selectionColor = color;
     return this;
+  }
+
+  /**
+   * Gets focus selection color
+   *
+   * @return Focus Selection color
+   */
+  public int getFocusSelectionColor() {
+    return this.focusSelectionColor;
   }
 
   /**
