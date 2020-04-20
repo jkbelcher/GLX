22912a8fe2f663d1e45c57123d9f7da725bdb299
Mark Slee <mcslee@gmail.com>
2020-04-20 15:17:02 -0700

    Add context menu direction options

--- src/main/java/heronarts/glx/ui/component/UIContextButton.java
+++ src/main/java/heronarts/glx/ui/component/UIContextButton.java
@@ -31,6 +31,22 @@ public class UIContextButton extends UI2dComponent implements UIFocus {
   private String label = "";
   private final UIContextMenu contextMenu;
   private float contextMenuWidth = -1;
+  private Direction direction = Direction.DOWN;
+
+  /**
+   * Direction that a context menu opens from a button
+   */
+  public enum Direction {
+    /**
+     * Menu opens beneath the button
+     */
+    DOWN,
+
+    /**
+     * Menu opens upwards from bottom of button
+     */
+    UP
+  };
 
   public UIContextButton(float x, float y, float w, float h) {
     super(x, y, w, h);
@@ -40,25 +56,60 @@ public class UIContextButton extends UI2dComponent implements UIFocus {
     this.contextMenu = new UIContextMenu(0, 0, UIContextMenu.DEFAULT_WIDTH, 0);
   }
 
+  /**
+   * Accessor for the underlying context menu object
+   *
+   * @return Context menu object opened by this button
+   */
   public UIContextMenu getContextMenu() {
     return this.contextMenu;
   }
 
+  /**
+   * Sets the width of the context menu opened by this button
+   *
+   * @param contextMenuWidth Width of context menu
+   * @return this
+   */
   public UIContextButton setContextMenuWidth(float contextMenuWidth) {
     this.contextMenuWidth = contextMenuWidth;
     return this;
   }
 
+  /**
+   * Sets an array of actions that will be shown in the context menu that opens when
+   * the button is clicked
+   *
+   * @param contextActions Set of context actions shown when button is clicked
+   * @return this
+   */
   public UIContextButton setContextActions(UIContextActions.Action[] contextActions) {
     this.contextMenu.setActions(contextActions);
     return this;
   }
 
+  /**
+   * Sets the label visible on the button
+   *
+   * @param label Label
+   * @return this
+   */
   public UIContextButton setLabel(String label) {
     if (!this.label.equals(label)) {
       this.label = label;
       redraw();
     }
+    return this;
+  }
+
+  /**
+   * Sets the direction in which the context menu opens
+   *
+   * @param direction Direction to open
+   * @return this
+   */
+  public UIContextButton setDirection(Direction direction) {
+    this.direction = direction;
     return this;
   }
 
@@ -111,7 +162,14 @@ public class UIContextButton extends UI2dComponent implements UIFocus {
 
   private void showMenu() {
     this.contextMenu.setWidth((this.contextMenuWidth > 0) ? this.contextMenuWidth : Math.max(UIContextMenu.DEFAULT_WIDTH, this.width));
-    this.contextMenu.setPosition(this, 0, this.height);
+    switch (this.direction) {
+    case DOWN:
+      this.contextMenu.setPosition(this, 0, this.height);
+      break;
+    case UP:
+      this.contextMenu.setPosition(this, 0, -this.contextMenu.getHeight());
+      break;
+    }
     this.contextMenu.setHighlight(0);
     getUI().showContextOverlay(this.contextMenu);
   }
